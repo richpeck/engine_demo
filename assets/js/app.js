8411023bfb5cdd2b70b1e4c0143df821411e707c
richpeck <rpeck@frontlineutilities.co.uk>
2022-11-03 07:46:39 +0000

    Styling etc

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+/*
+    ----------------------------
+    ---------------------------
+          ___              
+         / _ \             
+        / /_\ \_ __  _ __  
+        |  _  | '_ \| '_ \ 
+        | | | | |_) | |_) |
+        \_| |_/ .__/| .__/ 
+            | |   | |    
+            |_|   |_|    
+
+    ----------------------------
+    ----------------------------
+
+    Main ingress point into the system - used to provide functionality, game loop etc.
+
+    ----------------------------
+    ----------------------------
+
+ */
+
+// Start
+// This is the main ingress point and should be used to provide functionality for the app.
+document.addEventListener("DOMContentLoaded", function(event) { 
+    
+    console.log('test');
+
+});
